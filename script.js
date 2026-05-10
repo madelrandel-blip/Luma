1613cb6d179019dd1db85bc9d5c61984ae319455
Mad <madelrandel@gmail.com>
2026-05-10 13:21:56 -0700

    Test Cards/Buttons regression

--- script.js
+++ script.js
@@ -241,7 +241,6 @@ function abrirLink(link){
     }
 }
 
-/* ========= RENDER JUEGOS ========= */
 function render(lista){
     if(!store) return;
 
@@ -250,19 +249,28 @@ function render(lista){
     const inicio = (paginaActual - 1) * juegosPorPagina;
     const fin = inicio + juegosPorPagina;
 
+    // IMPORTANTE: esto faltaba
     const juegosPagina = lista.slice(inicio, fin);
 
     juegosPagina.forEach(j => {
         html += `
         <div class="card">
             <img src="${j.img}" loading="lazy" decoding="async" alt="${j.nombre}">
+
             <div class="content">
-                <h3>${j.nombre}</h3>
-                <p>${j.desc}</p>
+                <div class="info-overlay">
+                    <h3>${j.nombre}</h3>
+                    <p>${j.desc}</p>
+                </div>
 
                 <div class="btns">
-                    <button class="btn blue" onclick="playClick();abrirLink('${j.link1}')">Ver enlace</button>
-                    <button class="btn green" onclick="playClick();abrirLink('${j.link2}')">Ver enlace</button>
+                    <button class="btn blue" onclick="playClick();abrirLink('${j.link1}')">
+                        Ver enlace
+                    </button>
+
+                    <button class="btn green" onclick="playClick();abrirLink('${j.link2}')">
+                        Ver enlace
+                    </button>
                 </div>
 
                 ${admin ? `
